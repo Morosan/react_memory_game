e940c020a7b68e216d28a9766fe8d526c920fa7a
Andrei M <morosan.ionutandrei@gmail.com>
2023-02-05 15:11:56 +0200

    update with clapping sound, sneekpeek function and adding 4 characters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import clapping from '../public/sounds/clapping'
+import clapping from './sounds/clapping.wav'
 import SingleCard from './components/SingleCard/SingleCard';
-// import SelectControl from './components/SelectControl/SelectControl';
 
 const cardImages = [
   {
@@ -29,6 +28,22 @@ const cardImages = [
     "src": "/img/sven.jpg",
     "tag": "default"
   },
+  {
+    "src": "/img/hans.jpg",
+    "tag": "default"
+  },
+  {
+    "src": "/img/oaken.jpg",
+    "tag": "default"
+  },
+  {
+    "src": "/img/snowgies.jpg",
+    "tag": "default"
+  },
+  {
+    "src": "/img/marshmallow.jpg",
+    "tag": "default"
+  },
 ]
 
 function App() {
@@ -37,16 +52,17 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setdisabled] = useState(false);
-  const [selectedTheme, setSelectedTheme] = useState('defaultTheme'); 
-  // const [selectedTag, setSelectedTag] = useState('');
+  const [flipped, setFlipped] = useState(false);
 
-  //filter cards
-  // const filterCards = () => {
-  //   setCards(prevCards => {
-  //     return prevCards.filter(card => card.tag === selectedTag);
-  //   });
-  // };
-  
+  //render cards
+  const renderCards = (cards) => {
+    setCards(cards);
+  }
+
+  const showToMemorize = () => {
+    setFlipped(true);
+    setTimeout(() => setFlipped(false), 3000);
+  }
 
   //shuffle cards 
   const shuffleCards = () => {
@@ -56,14 +72,14 @@ function App() {
 
       setChoiceOne(null);
       setChoiceTwo(null);
-      setCards(shuffleCards);
+      renderCards(shuffleCards);
       setTurns(0);
-      // filterCards();
   }
 
   //handle a choice
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    console.log("handleChoice");
   }
 
   //compare 2 selected cards
@@ -88,7 +104,7 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
-  //reset choices & increse turn
+  // reset choices & increse turn
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
@@ -101,27 +117,37 @@ function App() {
     new Audio(clapping).play()
   }
 
-  // check if all cards are flipped
+  // check if all cards are flipped function
   const checkFlipped = () => {
-    
+    const allCardsFlipped = cards.every(card => card.matched === true);
+    console.log("turns",turns)
+    if (allCardsFlipped && turns !== 0) {
+      startClapping()
+    }
   }
 
-  // useEffect(() => {
-  //   filterCards();
-  // }, [selectedTag]);
+  // call the checkFlipped
+  useEffect(() => {
+    checkFlipped()
+  }, [choiceOne, choiceTwo, cards])
 
   //start a new game automatically
   useEffect(() => {
     shuffleCards()
-  }, [])
+    showToMemorize()
+  }, []) 
+  
+  //start a new game by clicking on the New game button
+  const resetGame = () => {
+    shuffleCards()
+    showToMemorize()
+  }
 
   return (
     <div className="App">
       <h1>Memory Game</h1>
       
-      {/* <SelectControl selectedTheme={selectedTheme} onChange={setSelectedTheme}/> */}
-
-      <button onClick={shuffleCards}>New Game</button>
+      <button onClick={resetGame}>New Game</button>
 
       <div className="card-grid">
         {cards
@@ -130,11 +156,11 @@ function App() {
               key={card.id}
               card={card}
               handleChoice={handleChoice}
-              flipped={card === choiceOne || card === choiceTwo || card.matched}
+              flipped={flipped || card === choiceOne || card === choiceTwo || card.matched}
               disabled={disabled}
             />
           ))
-          .slice(0, 12)
+          .slice(0, 20)
       }
       </div>
 
--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -4,21 +4,19 @@ import './SingleCard.css';
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
     const handleClick = () => {
         if (!disabled) {
-            handleChoice(card)
+            handleChoice(card);
         }
     }
-  
+
     return (
-    <div className="card">
-        <div className={flipped ? "flipped" : ""}>
-            <img src={card.src} alt={card.src} className="front" />
-            <img 
-                src="/img/snowflake.jpg" 
-                alt="card back" 
-                className="back" 
-                onClick={handleClick}
-            />
-        </div>
+    <div className={flipped ? "card flipped" : "card"}>
+        <img src={card.src} alt={card.src} className="front" />
+        <img 
+            src="/img/snowflake.jpg" 
+            alt="card back" 
+            className="back" 
+            onClick={handleClick}
+        />
     </div>
   )
 }
